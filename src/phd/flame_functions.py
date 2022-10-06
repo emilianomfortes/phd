cb5bb9f6e55d56d5595e5455701d0a88796b38e4
emilianomfortes <emilianomfortes@hotmail.com>
2022-10-06 06:18:31 +0200

    created function df_paraview_to_fg and added gitignore

--- src/phd/flame_functions.py
+++ src/phd/flame_functions.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-
+# Flame functions
 def laminar_flame_speed(v_u, v_b, rho_u, rho_b, stationary=False):
     """Calculates the laminar flame speed s_l for a 1d flame."""
     if stationary:
@@ -11,3 +11,16 @@ def laminar_flame_speed(v_u, v_b, rho_u, rho_b, stationary=False):
         # There's an inlet of unburnt gas
         flame_speed = (v_b - v_u) / (rho_u / rho_b - 1)
     return flame_speed
+
+
+# Dataframe functions
+def df_paraview_to_fg(df, dim=3):
+    """Translates column names from paraview .csv file to fg format"""
+    possible_cols = ["VELOC:0", "Points:0", "DENSI", "TEMPE", "ZMEAN", "CMEAN"]
+    flamegen_cols = ["u(m/s)", "x(m)", f"rho(kg/{dim})", "T(K)", "Z", "C"]
+    df_cols = list(df.columns)
+
+    for col, fg_col in zip(possible_cols, flamegen_cols):
+        if col in df_cols:
+            df = df.rename(columns={col: fg_col})
+    return df 
